9b218e65bab55c16d8e658aab9d4dfa028dbce87
Refael <Refaelasraf@gmail.com>
2020-04-14 02:23:23 +0300

    first commit

---
+++ src/pages/SignUp.vue
@@ -0,0 +1,19 @@
+<template>
+    <signup-form></signup-form>
+</template>
+
+<script>
+    import SignupForm from "./components/SignupForm";
+    import MainFooter from '@/layout/MainFooter';
+    export default {
+        name: "SignUp",
+        components: {
+            MainFooter,
+            SignupForm,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
