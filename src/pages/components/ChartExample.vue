60c2ba7c3c9aa6d7debdd24c46db59ed563352d7
Refael <Refaelasraf@gmail.com>
2020-04-15 18:08:34 +0300

    add chart for examples

---
+++ src/pages/components/ChartExample.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ChartExample"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/components/ThirdChartSample.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ThirdChartSample"
+    }
+</script>
+
+<style scoped>
+
+</style>
