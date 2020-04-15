3215df4887cb6f32b6045eb97a83870d51fddc2a
Refael <Refaelasraf@gmail.com>
2020-04-15 18:31:23 +0300

    change sign up page

--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -1,17 +1,27 @@
 <template>
     <div>
-    <signup-form></signup-form>
+        <div class="page-header clear-filter" filter-color="orange">
+            <parallax
+                    class="page-header-image"
+                    style="background-image:url('img/bg11.jpg')"
+            >
+            </parallax>
+            <div class="container">
+
+            <signup-form></signup-form>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import SignupForm from "./components/SignupForm";
-    import MainFooter from '@/layout/MainFooter';
+    import Parallax from "../components/Parallax";
     export default {
         name: "SignUp",
         components: {
-            MainFooter,
             SignupForm,
+            Parallax
         }
     }
 </script>
