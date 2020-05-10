66c67fe1a666860c461d288bfb469a472b4c60b3
Tom <tomashyam@gmail.com>
2020-05-10 23:14:56 +0300

    fix aggregation

--- src/server/DAL/statisticsDAL.ts
+++ src/server/DAL/statisticsDAL.ts
@@ -1,48 +1,49 @@
 import { ElasticHelper } from "../dbHelpers/elasticHelper";
-import { purchesRecord } from "../models/purchesRecord";
 import { ESBaseDAL } from "./ElasticSearchDAL/esBaseDAL";
 import { ITransaction } from "../models/transaction";
 import { IESDALConfig } from "../configuration/IConfig";
+import { IUserStatistic } from "../models/userStatistic";
 
 export class statisticsDAL extends ESBaseDAL<ITransaction> {
     private readonly esConfig : IESDALConfig = {
-      index : "transaction",
+      index : "my_money",
       type : "_doc"      
     }
-    private readonly indexName = "my_money";
-    public constructor ( elasticHelper = new ElasticHelper({host:'localhost:9200'})){
+    public constructor ( elasticHelper = new ElasticHelper({host:'http://localhost:9200'})){
         super(elasticHelper, {
-          index : "transaction",
+          index : "my_money",
           type : "_doc"      
         });
     }
     
-    public getUserStatistics = async (userId : string) : Promise<object> => {
-        // const aggsResult = await elasticHelper.elasticClient.search<purchesRecord>({
-        //     size:0,
-        //     index : this.indexName,
-        //     body : {
-        //         "query" : {
-        //             "match" :{
-        //                 "userId" : userId
-        //             }
-        //         },
-        //         "aggs": {
-        //             "category" : {
-        //               "terms": {
-        //                 "field": "category"
-        //               },
-        //               "aggs": {
-        //                 "sum_money": {
-        //                   "sum": {
-        //                     "field": "ammount"
-        //                   }
-        //                 }
-        //               }
-        //             }
-        //           }  
-        //         }
-        //     });
-        return null;
+    public getUserStatistics = async (clientId : string) : Promise<Array<IUserStatistic>> => {
+       const aggsResult = await this.elasticHelper.aggregate(this.esConfig.index, this.esConfig.type, {
+          "query" : {
+            "match" : {
+              "clientId" : clientId
+            }
+          },
+          "aggs": {
+            "category" : {
+              "terms": {
+                "field": "category"
+              },
+              "aggs": {
+                "sum_money": {
+                  "sum": {
+                    "field": "ammount"
+                  }
+                }
+              }
+            }
+          }
+        }          )
+        const ress = aggsResult.category.buckets.map((bucket : any) => {
+          return {
+            category :bucket.key,
+            ammount : bucket.sum_money.value
+          }
+        })
+        return ress;
     }
 }
