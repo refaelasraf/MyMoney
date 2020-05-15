cfa481f039b32c127cd9508475e43cfdd524b076
Ohad <etsionohad@gmail.com>
2020-05-15 15:33:33 +0300

    Add user home page & navbar to it

---
+++ src/pages/UserHome.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="page-header page-header-small">
+      <parallax
+        class="page-header-image"
+        style="background-image: url('img/bg6.jpg')"
+      >
+      </parallax>
+      <div class="content-center">
+        <div class="container">
+          <h1 class="title">User Home</h1>
+        </div>
+      </div>
+    </div>
+        <div class="container">
+          <colum-chart></colum-chart>
+          <second-chart></second-chart>
+          <third-chart-sample></third-chart-sample>
+        </div>
+    </div>
+</template>
+<script>
+import { Button, FormGroupInput } from '@/components';
+import ColumChart from "./components/ColumChart";
+import SecondChart from "./components/SecondChart";
+import ThirdChartSample from "./components/ThirdChartSample";
+export default {
+  name: 'userhome',
+  bodyClass: 'user-home-page',
+  components: {
+    ThirdChartSample,
+    SecondChart,
+    ColumChart,
+    [Button.name]: Button,
+    [FormGroupInput.name]: FormGroupInput
+  },
+  data() {
+    return {
+      form: {
+        firstName: '',
+        email: '',
+        message: ''
+      }
+    };
+  }
+};
+</script>
+<style></style>
