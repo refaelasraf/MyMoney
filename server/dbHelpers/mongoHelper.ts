02d87018cda1943102e41962b34b76476830ca8c
Refael <Refaelasraf@gmail.com>
2020-05-23 17:12:55 +0300

    catche warning Error from mongo update request

--- server/dbHelpers/mongoHelper.ts
+++ server/dbHelpers/mongoHelper.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
  
 export default class MongoHelper {
   public static async connect(url: string = "mongodb://localhost/test") {
-    await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+    await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false})
   }
  
   public static async disconnect() {
