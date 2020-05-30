28c7bd3b20a6fdd6acf14cd18c1f272ae7a6d6bd
guy_halevy <guy_halevy88@gmail.com>
2020-05-30 14:24:06 +0300

    Add comments

--- server/BL/notificationBL.ts
+++ server/BL/notificationBL.ts
@@ -2,6 +2,7 @@ import * as webPush from "web-push";
 import {config} from "../configuration/config";
 import {IPushWebConfig} from "../configuration/IConfig";
 
+// Notification BL
 export class NotificationBL {
     constructor(webPushConfig: IPushWebConfig = config.pushWeb) {
         webPush.setVapidDetails("mailto:refaelasraf@gmail.com", webPushConfig.publicKey, webPushConfig.privateKey);
