95b5d7acbe8e0f15e65e0455cbf55b9902ce634e
Meirav Shenhar <meinhh@gmail.com>
2020-06-03 16:34:56 +0300

    heruko please work pleaseeee

--- server/server.ts
+++ server/server.ts
@@ -20,7 +20,7 @@ import path from 'path';
 import serveStatic from 'serve-static';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(Cors());
 app.listen(port, ()=> console.log(`app is running on port ${port}`));
 
