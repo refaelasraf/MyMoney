677cd69b613eed9e503111c6f11354b30e8ec848
Ohad <etsionohad@gmail.com>
2020-06-10 18:36:57 +0300

    Add fields to SignUp From

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -4,9 +4,9 @@ import {config} from "../configuration/config";
 export default class UserService {
     static #route = `${config.baseServerUrl}/api/user`;
 
-    static async register(userName, password, dateOfBirth, email, city, DistenceFromWork, numOfPersonsToTakeCareOf) {
+    static async register(userName, password, dateOfBirth, email, city, DistenceFromWork, numOfPersonsToTakeCareOf, numOfProviders, studyYears, netoIncome, taxesPayment, socialCarePayment) {
         try {
-        let res = await axios.post(this.#route + '/register', {userName, password, dateOfBirth, email, city, DistenceFromWork, numOfPersonsToTakeCareOf});
+        let res = await axios.post(this.#route + '/register', {userName, password, dateOfBirth, email, city, DistenceFromWork, numOfPersonsToTakeCareOf, numOfProviders, studyYears, netoIncome, taxesPayment, socialCarePayment});
         return res.data;
         } catch (ex) {
             return;
