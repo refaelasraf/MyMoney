533a2adcd6b1b1d0eb3e82375477eaaa490f246e
Halevy87 <64663805+Halevy87@users.noreply.github.com>
2020-06-10 17:55:22 +0300

    mail sender

--- server/configuration/config.ts
+++ server/configuration/config.ts
@@ -5,13 +5,21 @@ export var config: IConfig = {
         elasticsearch: {
             host: "http://193.106.55.123:9200"
         },
-        transactionDal:{
+        transactionDal: {
             index: "transactions",
-            type:"_doc"
+            type: "_doc"
         }
     },
-    pushWeb:{
-        publicKey:"BFxVLNQ0jH3Zm9tJjiREpWs344wKZ3SOesnKycXW5v6n0m88l7AxlAps2A05HmGVzI0QXrr6_BPObAPGH-6mNYA",
-        privateKey:"k-a0F5dx6-IoQPiXZUUGxgsWLb7fdJqiQ86HP6fJ5r8"
+    pushWeb: {
+        publicKey: "BFxVLNQ0jH3Zm9tJjiREpWs344wKZ3SOesnKycXW5v6n0m88l7AxlAps2A05HmGVzI0QXrr6_BPObAPGH-6mNYA",
+        privateKey: "k-a0F5dx6-IoQPiXZUUGxgsWLb7fdJqiQ86HP6fJ5r8"
+    },
+    mail: {
+        service: "gmail",
+        auth: {
+            pass: "myMoney123",
+            user: "mymoneycolman@gmail.com"
+        }
+
     }
 }
