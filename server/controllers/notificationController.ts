8f972300c61064be2dfe39f8e1f44f21c82286d9
Refael <Refaelasraf@gmail.com>
2020-07-21 18:48:49 +0300

    change to user Name from user id

--- server/controllers/notificationController.ts
+++ server/controllers/notificationController.ts
@@ -2,25 +2,26 @@ import {Router} from "express";
 import {SubscriptionBL} from "../BL/subscriptionBL";
 import {ISubscription} from "../models/subscription";
 import {NotificationBL} from "../BL/notificationBL";
+import {UserBL} from "../BL/userBL";
 
 export class NotificationController {
     private router = Router();
-    constructor(private subscriptionBL: SubscriptionBL = new SubscriptionBL(), notificationBL: NotificationBL = new NotificationBL()) {
+    constructor(private subscriptionBL: SubscriptionBL = new SubscriptionBL(), notificationBL: NotificationBL = new NotificationBL(),private userBL:UserBL = new UserBL()) {
 
-        this.router.post('/subscribe', (req, res) => {
+        this.router.post('/subscribe', async (req, res) => {
             const webSubscription = req.body.subscription;
             const userId = req.body.userId;
-
+            const user = await userBL.getById(req.body.userId)
             const subMessage: ISubscription = {
                 owner : userId,
                 subscription:webSubscription
             }
 
-            subscriptionBL.add(subMessage);
+            await subscriptionBL.add(subMessage);
 
             res.status(201).json({});
 
-            notificationBL.sendNotification(webSubscription, userId, "Wellcome user");
+            notificationBL.sendNotification(webSubscription, user.userName, "Wellcome ");
         });
 
     }
