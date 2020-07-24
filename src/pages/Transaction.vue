505146173ada9ecae09b4300be2fa7a3cd1ff8e7
Refael <Refaelasraf@gmail.com>
2020-07-24 17:45:11 +0300

    add option to add transaction

--- src/pages/Transaction.vue
+++ src/pages/Transaction.vue
@@ -19,33 +19,86 @@
             </div>
         </div>
         <div class="content-center">
+            <modal :show.sync="modals.add.isVisible" headerClasses="justify-content-center">
+                <h4 slot="header" class="title title-up">Add Transaction</h4>
+                <fg-input placeholder="Store Name" maxlength="10" v-model="modals.add.transaction.storeName"></fg-input>
+                <fg-input placeholder="amount" type="number"   v-model="modals.add.transaction.amount"></fg-input>
+                <el-date-picker v-model="modals.add.transaction.eventTime"
+                                popper-class="date-picker-primary"
+                                type="date"
+                                placeholder="Start Date...">
+                </el-date-picker>
+                <el-select class="select-danger"
+                           placeholder="Single Select"
+                           v-model="modals.add.transaction.categoryId">
+                    <el-option v-for="option in categories.all"
+                               class="select-danger"
+                               :value="option.value"
+                               :label="option.label"
+                               :key="option.label">
+                    </el-option>
+                </el-select>
+                <template slot="footer">
+                    <n-button @click="addTransaction">Add Transaction</n-button>
+                    <n-button type="danger" @click.native="modals.add.isVisible = false">Close</n-button>
+                </template>
+            </modal>
             <div class="container">
                 <transaction-table v-bind:transactions="transactions"/>
                 <br/>
                 <div>
                     Total spending in month {{totalSpending}}$
-                </div>
+                </div><br/>
+                <n-button @click.native="modals.add.isVisible = true">Add Transaction</n-button>
             </div>
         </div>
     </div>
 </template>
 <script>
     import TransactionTable from "./components/TransactionTable";
-    import {DatePicker} from "element-ui";
     import TransactionService from "../services/TransactionService";
-
+    import {Option, Select} from "element-ui"
+    import {Button, FormGroupInput, Modal} from '@/components';
+    import {DatePicker} from 'element-ui';
     export default {
         name: 'transaction',
         bodyClass: 'Transaction-page',
         components: {
             TransactionTable,
             [DatePicker.name]: DatePicker,
+            [Button.name]: Button,
+            [FormGroupInput.name]: FormGroupInput,
+            [Modal.name]: Modal,
+            [Select.name]: Select,
+            [Option.name]: Option
         },
         data() {
             return {
                 date: null,
                 transactions: null,
                 totalSpending: 0,
+                categories: {
+                    all: [
+                        {value: 'clothing', label: 'clothing'},
+                        {value: 'food', label: 'food'},
+                        {value: 'electronics', label: 'electronics'},
+                        {value: 'fule', label: 'fule'},
+                        {value: 'culture', label: 'culture'}]
+                },
+                modals: {
+                    classic: false,
+                    editProfile: false,
+                    add: {
+                        isVisible: false,
+                        transaction: {
+                            categoryId: "food",
+                            amount: 0,
+                            storeName: "Store Name",
+                            clientId: localStorage.userId,
+                            eventTime: Date.now()
+                        }
+                    }
+                }
             }
         },
         created() {
@@ -55,6 +108,16 @@
             this.getTransactions(dt);
         },
         methods: {
+            async addTransaction(){
+                await TransactionService.upsert(this.modals.add.transaction);
+                this.modals.add.isVisible = false
+                this.modals.add.transaction.categoryId = "food";
+                this.modals.add.transaction.eventTime = Date.now();
+                this.modals.add.transaction.amount = 0;
+                this.modals.add.transaction.storeName = "Store Name"
+                this.date = dt;
+                await this.getTransactions(this.date);
+            },
             async getTransactions(event) {
                 this.transactions = await TransactionService.getMonthByUser(event);
                 this.totalSpending = this.transactions.map(t => t.amount).reduce((accum, curr) => accum + curr, 0);
--- src/services/TransactionService.js
+++ src/services/TransactionService.js
@@ -1,11 +1,14 @@
 import axios from "axios"
 import {config} from "../configuration/config";
+import { v4 as uuidv4 } from 'uuid';
 
 export default class TransactionService {
     static #route = `${config.baseServerUrl}/api/transaction`;
 
-    static async getMonth(date) {
-        let res = await axios.post(this.#route + '/getByDate', {start: date, end: this.getNextMonth(date)});
+    static async upsert(transaction) {
+        transaction.docID = uuidv4();
+        debugger;
+        let res = await axios.post(this.#route + '/upsert', {transaction});
         return res.data;
     }
 
