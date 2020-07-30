24aecf5d7f53d19e6982f2d20023e81babd07ca5
Refael <Refaelasraf@gmail.com>
2020-07-30 13:50:35 +0300

    not fall when have litlle exceptions

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,11 @@
+process
+    .on('unhandledRejection', (reason, p) => {
+        console.error(reason, 'Unhandled Rejection at Promise', p);
+    })
+    .on('uncaughtException', err => {
+        console.error(err, 'Uncaught Exception thrown');
+    });
+
 import express from "express";
 import {config} from "./configuration/config";
 import {ElasticHelper} from "./dbHelpers/elasticHelper";
@@ -18,12 +26,12 @@ import SubscriptionController from "./controllers/subscriptionController";
 import {GoalChecker} from "./service/GoalChecker";
 import path from 'path';
 import serveStatic from 'serve-static';
-import { CategoriesController } from "./controllers/categoriesController";
+import {CategoriesController} from "./controllers/categoriesController";
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(Cors());
-app.listen(port, ()=> console.log(`app is running on port ${port}`));
+app.listen(port, () => console.log(`app is running on port ${port}`));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -58,23 +66,23 @@ app.use("/api/subscription", subscriptionRouter);
 
 //user
 app.post("/api/user/register", (req, res) => userC.register(req, res));
-app.post("/api/user/login", (req, res) =>userC.login(req, res));
-app.get("/api/user/setUserAsAdmin/:id" , (req, res) => userC.setUserAsAdmin(req, res));
-app.get("/api/user/getById/:id" , (req, res) => userC.getById(req, res));
-app.get("/api/user/load" , (req, res) => userC.loadUsers(req, res));
-app.post("/api/user/edit" , (req, res) => userC.edit(req, res));
+app.post("/api/user/login", (req, res) => userC.login(req, res));
+app.get("/api/user/setUserAsAdmin/:id", (req, res) => userC.setUserAsAdmin(req, res));
+app.get("/api/user/getById/:id", (req, res) => userC.getById(req, res));
+app.get("/api/user/load", (req, res) => userC.loadUsers(req, res));
+app.post("/api/user/edit", (req, res) => userC.edit(req, res));
 
 //credit card
-app.post("/api/creditCard/add", (req, res) =>creditCardC.add(req, res));
-app.post("/api/creditCard/edit", (req, res) =>creditCardC.edit(req, res));
-app.get("/api/creditCard/remove/:id", (req, res) =>creditCardC.remove(req, res));
-app.get("/api/creditCard/getByUser/:userId", (req, res) =>creditCardC.getByUser(req, res));
+app.post("/api/creditCard/add", (req, res) => creditCardC.add(req, res));
+app.post("/api/creditCard/edit", (req, res) => creditCardC.edit(req, res));
+app.get("/api/creditCard/remove/:id", (req, res) => creditCardC.remove(req, res));
+app.get("/api/creditCard/getByUser/:userId", (req, res) => creditCardC.getByUser(req, res));
 
 //bank account
-app.post("/api/bankAccount/add", (req, res) =>bankAccountC.add(req, res));
-app.post("/api/bankAccount/edit", (req, res) =>bankAccountC.edit(req, res));
-app.get("/api/bankAccount/remove/:id", (req, res) =>bankAccountC.remove(req, res));
-app.get("/api/bankAccount/getByUser/:userId", (req, res) =>bankAccountC.getByUser(req, res));
+app.post("/api/bankAccount/add", (req, res) => bankAccountC.add(req, res));
+app.post("/api/bankAccount/edit", (req, res) => bankAccountC.edit(req, res));
+app.get("/api/bankAccount/remove/:id", (req, res) => bankAccountC.remove(req, res));
+app.get("/api/bankAccount/getByUser/:userId", (req, res) => bankAccountC.getByUser(req, res));
 
 //statistics
 app.get("/api/statistics/getUserStats/:userId", statisticsController.getUserStatistics);
@@ -85,9 +93,9 @@ app.get("/api/admin/getStats");
 app.get("/api/admin/getUsers", (req, res) => adminController.getUsers(req, res));
 
 //category
-app.get("/api/categories/getCategories", (req,res) => categoriesController.getCategoriesList(req,res))
+app.get("/api/categories/getCategories", (req, res) => categoriesController.getCategoriesList(req, res))
 
-function  createTransactionRouter() {
+function createTransactionRouter() {
     let elasticHelper = new ElasticHelper(config.DAL.elasticsearch);
     let transactionDAL = new TransactionDAL(elasticHelper, config.DAL.transactionDal);
     let transactionBL = new TransactionBL(transactionDAL);
